69fc4552adc6fbab2961dc0edefcbdab95badd72
wangzhe <wangzhe>
2018-04-26 10:38:31 +0800

    设备号判断

--- src/main/java/com/ybyc/skids/tbox/hikvision/HikvisionFactory.java
+++ src/main/java/com/ybyc/skids/tbox/hikvision/HikvisionFactory.java
@@ -5,6 +5,7 @@ import com.ybyc.skids.core.Car;
 import com.ybyc.skids.core.TboxFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
 
 @Component("hktboxFactory")
 public class HikvisionFactory implements TboxFactory {
@@ -14,6 +15,8 @@ public class HikvisionFactory implements TboxFactory {
 
     @Override
     public BaseTbox build(Car car) {
+        Assert.notNull(car.getId(),"设备号错误");
+        Assert.isTrue(car.getId().length()==11,"设备号应该是11位");
         return new HikvisionTbox(car,hikvisionContext);
     }
 }
--- src/main/java/com/ybyc/skids/tbox/threadcn/ThreadcnFactory.java
+++ src/main/java/com/ybyc/skids/tbox/threadcn/ThreadcnFactory.java
@@ -5,6 +5,7 @@ import com.ybyc.skids.core.Car;
 import com.ybyc.skids.core.TboxFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
 
 @Component("srdtboxFactory")
 public class ThreadcnFactory implements TboxFactory {
@@ -14,6 +15,8 @@ public class ThreadcnFactory implements TboxFactory {
 
     @Override
     public BaseTbox build(Car car) {
+        Assert.notNull(car.getId(),"设备号错误");
+        Assert.isTrue(car.getId().length()==12,"设备号应该是12位，形如：E61254B88755");
         return new ThreadcnTbox(car,context);
     }
 
