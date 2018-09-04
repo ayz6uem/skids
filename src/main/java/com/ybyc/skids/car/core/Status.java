015fd6b7f7f202f8edd0ec4843af2d3476c1b86e
wangzhe <wangzhe>
2018-09-04 16:54:56 +0800

    移除电量处理，交由上层处理

--- src/main/java/com/ybyc/skids/car/core/Status.java
+++ src/main/java/com/ybyc/skids/car/core/Status.java
@@ -19,12 +19,10 @@ public class Status {
     private boolean lock;
 
     public void incOdo(double odo){
-        odo = new BigDecimal(odo).setScale(2,BigDecimal.ROUND_HALF_UP).doubleValue();
         this.odo += odo;
     }
 
     public void incSoc(double soc){
-        soc = new BigDecimal(soc).setScale(2,BigDecimal.ROUND_HALF_UP).doubleValue();
         this.soc += soc;
         if(this.soc>100){
             this.soc = 100;
@@ -32,7 +30,6 @@ public class Status {
     }
 
     public void decSoc(double soc){
-        soc = new BigDecimal(soc).setScale(2,BigDecimal.ROUND_HALF_UP).doubleValue();
         this.soc -= soc;
         if(this.soc<0){
             this.soc = 0;
