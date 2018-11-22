0c2781b03a4456336a42359a9418908f8dce75d2
wangzhe <wangzhe@1byongche.com>
2018-11-22 15:49:43 +0800

    主动停止充电

--- src/main/java/com/ybyc/skids/charge/StationController.java
+++ src/main/java/com/ybyc/skids/charge/StationController.java
@@ -6,16 +6,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+
 @RestController
 @RequestMapping("/charge/station")
 public class StationController {
 
-    @Autowired
+    @Resource
     ChargeService chargeService;
+
+    @Resource
+    OrderService orderService;
 
     @RequestMapping
     public ResultData status(String connectorId, ConnectorStatusEnum statusEnum){
         chargeService.stationStatusNotify(connectorId,statusEnum);
+        return ResultData.ok();
+    }
+
+    @RequestMapping("stop")
+    public ResultData stop(String sn,String connectorId){
+        orderService.stop(sn, connectorId);
         return ResultData.ok();
     }
 
